53cb0baa4c9c1accda0c94459bcc309c73dae3c0
tavisca-samishra <samishra@tavisca.in>
2019-06-19 18:20:59 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Grade
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10, b = 20.1;
+            double c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
